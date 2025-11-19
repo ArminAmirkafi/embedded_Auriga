0ceb8febeb90d30d5e88db23d632b9c3bb3042e2
Armin_Amirkafii <a.amirkafi05@gmail.com>
2025-11-19 08:05:47 -0800

    ggjfj

--- uart_lib/components/my_uart/include/my_uart.h
+++ uart_lib/components/my_uart/include/my_uart.h
@@ -8,12 +8,18 @@
 #include "driver/uart.h"
 
 #define BUF_SIZE 1024
+#define UART_START 0x7E
+#define UART_DONE   0x7F
+//function 
+void uart_init(gpio_port_t port, int baud_rate,uart_event_t* event_handle);
+void uart_printf(const char* _msg,...);//omid will put his kir in it
+void uart_receive();
+void uart_event_handler();
+printf();
+void compress();
+void decompress();
+void crc16();
+void rx_flush();
+void rx();
 
-typedef struct{
-    int buad_rate;
-    uart_port_t port;
-    bool do_you_need_event_handling;
-
-} uart_setup_t;
-
-void uart_setup(void*);
+#endif 
--- uart_lib/components/my_uart/my_uart.c
+++ uart_lib/components/my_uart/my_uart.c
@@ -1,17 +1,20 @@
 #include "my_uart.h"
 
-QueueHandle_t queue=NULL;
-
-uart_setup(void *setup_ptr){
-    uart_setup_t* uart_setup_p=(uart_setup_t*)setup_ptr;
+QueueHandle_t uart_event=NULL;
+    
+uart_init(gpio_port_t port, int baud_rate){
     uart_config_t uart_conf={
-        .baud_rate=uart_setup_p->buad_rate,
+        .baud_rate=buad_rate,
         .parity=UART_PARITY_ENABLE,
         .stop_bits=UART_STOP_BITS_1,
-        .source_clk=
+        .flow_ctrl=UART_HW_FLOWCTRL_DISABLE,
+        .data_bits=UART_DATA_8_BITS
     }   
-    if(uart_setup_p->do_you_need_event_handling){
-        queue=
-    }
+    uart_param_config(port,&uart_conf);
+    uart_driver_install(port,BUF_SIZE*2,0,0,uart_event,0);
+}
 
+uart_printf(const char* _msg,...){
+    __va_list args;
+    va_st
 }
